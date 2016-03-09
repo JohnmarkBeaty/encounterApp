04019a59167742421c2b87168b4e8e017410811e
bsdymit <brad.dymit@gmail.com>
2016-03-09 17:17:19 -0600

    Tabs added
    Four tabs with css and js support added

--- www/js/index.js
+++ www/js/index.js
@@ -46,4 +46,18 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
+
+    jQuery(document).ready(function() {
+    jQuery('.tabs .tab-links a').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+
+        // Show/Hide Tabs
+        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+        e.preventDefault();
+        });
+    });
 };
